924f34efc789a1f88f14a3a238d7e05012e90cf3
godChoi96 <oinkoink71@naver.com>
2022-05-13 14:34:22 +0900

    blog API 시작

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,26 +1,19 @@
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from mysite.views import HomeView
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     # shkim
-#     path('', HomeView.as_view(), name='home'),
-#     path('blog/', include('blog.urls')),
-#     path('api/', include('api.urls')),
-#     # DRF
-#     path('api-auth/', include('rest_framework.urls')),
-# ]
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    path('api/', include('api.urls')),
     path('api2/', include('api2.urls')),
 
-    # path('', include(router.urls)),
     # 로그인 버튼 객체의 유무
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
